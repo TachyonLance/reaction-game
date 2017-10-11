96c4ca3c72653e1af5cd0e147329c52aaa8515a5
haydenc36 <haydenc36@yahoo.com>
2017-10-11 16:42:52 -0500

    Updating graphics

--- mainBranch/state0.js
+++ mainBranch/state0.js
@@ -5,42 +5,43 @@ demo.state0.prototype = {
     preload: function(){
         // The Matrix Raining
         // The button images
+
     },
     create:function(){
-        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-        
-        // Load the background
-        // var matrixRain = game.add.sprite(0,-100,'matrixRain');
-        // matrixRain.scale.set(0.45);
-        
-        var title = game.add.text(600, 200, "Word Cloud");
-        title.anchor.set(0.5,0.5);
-        title.align = 'center';
-        title.font = 'Book Antiqua';
-        title.fontSize = 100;
-        title.fontWeight = 'bold';
-        title.fontVariant = 'small-caps';
-        title.fontStyle = 'italic';
-        title.fill = '#fff';
-        title.setShadow(5, 0, 'rgba(0,0,0,0.5)', 0);
-        title.shadowBlur = 5;
-        
-        this.createButton(45, "Start Game", 600, 375, 400, 200, function(){
-            this.state.start("state1");
-        });
-        
-        this.createButton(25, "Instructions", 600, 500, 300, 100, function(){
-            this.state.start("state2");
-        });
-        
-        this.createButton(25, "Settings", 600, 600, 300, 100, function(){
-            this.state.start("state3");
-        });
-        
-        // 
-        this.createButton(25, "Leaderboards", 200, 375, 400, 200, function(){
-            this.state.start("state4", true, false, "../assets/BattleAssets.JSON", "BattleState");
-        });
+//        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+//        
+//        // Load the background
+//        // var matrixRain = game.add.sprite(0,-100,'matrixRain');
+//        // matrixRain.scale.set(0.45);
+//        
+//        var title = game.add.text(600, 200, "Word Cloud");
+//        title.anchor.set(0.5,0.5);
+//        title.align = 'center';
+//        title.font = 'Book Antiqua';
+//        title.fontSize = 100;
+//        title.fontWeight = 'bold';
+//        title.fontVariant = 'small-caps';
+//        title.fontStyle = 'italic';
+//        title.fill = '#fff';
+//        title.setShadow(5, 0, 'rgba(0,0,0,0.5)', 0);
+//        title.shadowBlur = 5;
+//        
+//        this.createButton(45, "Start Game", 600, 375, 400, 200, function(){
+//            this.state.start("state1");
+//        });
+//        
+//        this.createButton(25, "Instructions", 600, 500, 300, 100, function(){
+//            this.state.start("state2");
+//        });
+//        
+//        this.createButton(25, "Settings", 600, 600, 300, 100, function(){
+//            this.state.start("state3");
+//        });
+//        
+//        // 
+//        this.createButton(25, "Leaderboards", 200, 375, 400, 200, function(){
+//            this.state.start("state4", true, false, "../assets/BattleAssets.JSON", "BattleState");
+//        });
     },
     
     update: function(){
@@ -48,27 +49,27 @@ demo.state0.prototype = {
     },
     
     createButton:function(s, string, x, y, w, h, callback){
-        var btn = game.add.button(x,y,'button',callback,this,2,1,0);
-        
-        btn.anchor.setTo(0.5,0.5);
-        btn.width = w;
-        btn.height = h;
-        
-        var txt = game.add.text(btn.x, btn.y, string, {
-            font: "Book Antiqua",
-            fontStyle: "italic",
-            fontSize: s,
-            fontVariant: 'small-caps',
-            fontWeight:"bold",
-            fill:'#4d2800', 
-            align:'center'
-        });
-        
-        txt.setShadow(2.5, 0, 'rgba(0,0,0,0.5)', 0);
-        txt.shadowBlur = 5;
-        txt.anchor.setTo(0.5,0.5);
-        
-        btn.alpha = 0.9;
-        txt.alpha = 0.9;
+//        var btn = game.add.button(x,y,'button',callback,this,2,1,0);
+//        
+//        btn.anchor.setTo(0.5,0.5);
+//        btn.width = w;
+//        btn.height = h;
+//        
+//        var txt = game.add.text(btn.x, btn.y, string, {
+//            font: "Book Antiqua",
+//            fontStyle: "italic",
+//            fontSize: s,
+//            fontVariant: 'small-caps',
+//            fontWeight:"bold",
+//            fill:'#4d2800', 
+//            align:'center'
+//        });
+//        
+//        txt.setShadow(2.5, 0, 'rgba(0,0,0,0.5)', 0);
+//        txt.shadowBlur = 5;
+//        txt.anchor.setTo(0.5,0.5);
+//        
+//        btn.alpha = 0.9;
+//        txt.alpha = 0.9;
     }
 };
--- mainBranch/state1.js
+++ mainBranch/state1.js
@@ -1,1 +1,55 @@
 // Game State
+var cursors; 
+
+demo.state1 = function(){};
+demo.state1.prototype = {
+    
+    preload: function(){
+        
+        game.load.image('ocean', '../assets/ocean.png');
+
+    },
+    
+    create: function(){
+        
+        //Read in the file
+        readTextFile('../assets/testFile.txt');
+        
+        // Initialize Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        //Adjust camera settings
+        game.world.setBounds(0, 0, 2400, 2400);
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        game.add.sprite(0, 0, 'ocean');
+        
+        // Controls
+        cursors = game.input.keyboard.createCursorKeys();
+    },
+    
+    update: function(){
+        
+        
+        
+    }
+};
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
