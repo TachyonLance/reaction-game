6daadd5448ba3a8429677a6bdb5d48197df6d794
haydenc36 <haydenc36@yahoo.com>
2017-10-11 22:03:22 -0500

    Updates

--- mainBranch/state0.js
+++ mainBranch/state0.js
@@ -1,15 +1,65 @@
 
 // Main Menu
 var demo = {};
+
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
-        // The Matrix Raining
-        // The button images
+        
+        game.load.image('ocean', '../assets/ocean.png');
 
     },
+    
     create:function(){
-<<<<<<< Updated upstream
+    
+        game.add.sprite(0, 0, 'ocean');
+        
+        // c is the identifier of the canvas in HTML
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+
+        // Making the canvas full screen
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+
+        // Setting the character set and specifying text attributes
+        var english = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        english = english.split("");
+        var font_size = 10;
+        var columns = c.width/font_size;
+
+        // An array of drops - one per column
+        var drops = [];
+        for(var x = 0; x < columns; x++){
+            drops[x] = 1;}
+        
+        // Drawing the rain
+        function draw(){
+
+            // Set the background to be transparent
+            ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+            ctx.fillRect(0, 0, c.width, c.height);
+            ctx.fillStyle = "#c1d9ff";
+
+            for(var i = 0; i < drops.length; i++){
+                // A random english character to print
+                var text = english[Math.floor(Math.random()*english.length)];
+                ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+                //sending the drop back to the top randomly after it has crossed the screen
+                //adding a randomness to the reset to make the drops scattered on the Y axis
+                if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                    drops[i] = 0;
+
+                //incrementing Y coordinate
+                drops[i]++;
+            }
+        }
+
+    setInterval(draw, 33);
+
+        
+        
 //        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
 //        
 //        // Load the background
@@ -44,41 +94,40 @@ demo.state0.prototype = {
 //        this.createButton(25, "Leaderboards", 200, 375, 400, 200, function(){
 //            this.state.start("state4", true, false, "../assets/BattleAssets.JSON", "BattleState");
 //        });
-=======
-        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-        // Load the background
-        // var matrixRain = game.add.sprite(0,-100,'matrixRain');
-        // matrixRain.scale.set(0.45);
-        
-        var title = game.add.text(600, 200, "Word Cloud");
-        title.anchor.set(0.5,0.5);
-        title.align = 'center';
-        title.font = 'Book Antiqua';
-        title.fontSize = 100;
-        title.fontWeight = 'bold';
-        title.fontVariant = 'small-caps';
-        title.fontStyle = 'italic';
-        title.fill = '#fff';
-        title.setShadow(5, 0, 'rgba(0,0,0,0.5)', 0);
-        title.shadowBlur = 5;
-        
-        this.createButton(45, "Start Game", 600, 375, 400, 200, function(){
-            this.state.start("state1");
-        });
-        
-        this.createButton(25, "Instructions", 600, 500, 300, 100, function(){
-            this.state.start("state2");
-        });
-        
-        this.createButton(25, "Settings", 600, 600, 300, 100, function(){
-            this.state.start("state3");
-        });
-        
-        // 
-        this.createButton(25, "Leaderboards", 200, 375, 400, 200, function(){
-            this.state.start("state4", true, false, "../assets/BattleAssets.JSON", "BattleState");
-        });
->>>>>>> Stashed changes
+
+//        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+//        // Load the background
+//        // var matrixRain = game.add.sprite(0,-100,'matrixRain');
+//        // matrixRain.scale.set(0.45);
+//        
+//        var title = game.add.text(600, 200, "Word Cloud");
+//        title.anchor.set(0.5,0.5);
+//        title.align = 'center';
+//        title.font = 'Book Antiqua';
+//        title.fontSize = 100;
+//        title.fontWeight = 'bold';
+//        title.fontVariant = 'small-caps';
+//        title.fontStyle = 'italic';
+//        title.fill = '#fff';
+//        title.setShadow(5, 0, 'rgba(0,0,0,0.5)', 0);
+//        title.shadowBlur = 5;
+//        
+//        this.createButton(45, "Start Game", 600, 375, 400, 200, function(){
+//            this.state.start("state1");
+//        });
+//        
+//        this.createButton(25, "Instructions", 600, 500, 300, 100, function(){
+//            this.state.start("state2");
+//        });
+//        
+//        this.createButton(25, "Settings", 600, 600, 300, 100, function(){
+//            this.state.start("state3");
+//        });
+//        
+//        // 
+//        this.createButton(25, "Leaderboards", 200, 375, 400, 200, function(){
+//            this.state.start("state4", true, false, "../assets/BattleAssets.JSON", "BattleState");
+//        });
     },
     
     update: function(){
--- mainBranch/state1.js
+++ mainBranch/state1.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Game State
 var cursors, words; 
 
@@ -37,7 +36,6 @@ demo.state1.prototype = {
 };
 
 
-<<<<<<< HEAD
 // Reference text file for reading
 //$.ajax({
 //    url:'assets/testFile.txt',
@@ -73,7 +71,7 @@ console.log(words);
 words = words.map(v => v.toLowerCase());
 console.log(words);
 console.log(words[0], words[3]);
-=======
+
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
@@ -91,59 +89,4 @@ function readTextFile(file)
     }
     rawFile.send(null);
 }
-=======
-// c is the identifier of the canvas in .html
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
 
-//making the canvas full screen
-c.height = window.innerHeight;
-c.width = window.innerWidth;
-
-//chinese characters - taken from the unicode charset
-var chinese = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-//converting the string into an array of single characters
-chinese = chinese.split("");
-
-var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
-//an array of drops - one per column
-var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-	drops[x] = 1; 
-
-//drawing the characters
-function draw()
-{
-	//Black BG for the canvas
-	//translucent BG to show trail
-	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-	ctx.fillRect(0, 0, c.width, c.height);
-	
-	ctx.fillStyle = "#DDD"; //Color of text #0F0 for green
-	//ctx.font = font_size + "px arial";
-	//looping over drops
-	for(var i = 0; i < drops.length; i++)
-	{
-		//a random chinese character to print
-		var text = chinese[Math.floor(Math.random()*chinese.length)];
-		//x = i*font_size, y = value of drops[i]*font_size
-		ctx.fillText(text, i*font_size, drops[i]*font_size);
-		
-		//sending the drop back to the top randomly after it has crossed the screen
-		//adding a randomness to the reset to make the drops scattered on the Y axis
-		if(drops[i]*font_size > c.height && Math.random() > 0.975)
-			drops[i] = 0;
-		
-		//incrementing Y coordinate
-		drops[i]++;
-	}
-}
-
-setInterval(draw, 33);
-
-
->>>>>>> Stashed changes
->>>>>>> 1a9c69618fef46954e3835999e47ba1bd6d2cc5c
